435b9c75a49e21b5fa9b91daab5eae3443dbd5a5
HafizSyedAhmedAli <hafizsyedahmedali12@gmail.com>
2025-10-20 18:00:55 +0500

    Fixed last_name bug

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,12 +9,12 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
+    const { id, first_name, email_addresses, image_url } =
       event.data;
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + " " + last_name,
+      name: first_name,
       image: image_url,
     };
 
@@ -37,12 +37,12 @@ const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
+    const { id, first_name, email_addresses, image_url } =
       event.data;
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + " " + last_name,
+      name: first_name,
       image: image_url,
     };
 
