dc27fa39e25f8b610c3d792e9ffbe6879326a043
HafizSyedAhmedAli <hafizsyedahmedali12@gmail.com>
2025-11-01 17:36:46 +0500

    added / in herosection background image

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url(backgroundImage.png)] bg-cover bg-center h-screen">
+        <div className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url(/backgroundImage.png)] bg-cover bg-center h-screen">
             <img className="max-h-11 lg:h-11 mt-20" src={assets.marvelLogo} alt="" />
 
             <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110">Guardians <br /> of the Galaxy</h1>
